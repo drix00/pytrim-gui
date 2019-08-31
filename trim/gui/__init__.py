ecf7486da637e538b21bf47c5da04056bfe17edd
Hendrix Demers <hendrix.demers@mail.mcgill.ca>
2019-08-31 18:38:09 -0400

    Add project version

---
+++ trim/gui/__init__.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+.. py:currentmodule:: trim.gui
+
+.. moduleauthor:: Hendrix Demers <hendrix.demers@mail.mcgill.ca>
+
+
+"""
+# Copyright (C) 2019 Hendrix Demers
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# Standard library modules.
+
+# Third party modules.
+
+# Local modules.
+
+# Project modules.
+
+# Globals and constants variables.
+__author__ = 'Hendrix Demers'
+__email__ = 'hendrix.demers@mail.mcgill.ca'
+__version__ = '0.1.0'
+__copyright__ = '2019, ' + __author__
+__project_name__ = 'pyTRIM-GUI'
