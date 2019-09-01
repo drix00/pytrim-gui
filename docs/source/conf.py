93fa51d0d8c6b22bea9148d1192ddb7c54ed2a83
Hendrix Demers <hendrix.demers@mail.mcgill.ca>
2019-09-01 19:23:00 -0400

    Add the correct master_doc file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,6 @@ sys.path.insert(0, os.path.abspath('../../'))
 
 from trim.gui import __version__, __author__, __copyright__, __project_name__
 
-
 # -- Project information -----------------------------------------------------
 
 project = __project_name__
@@ -33,6 +32,9 @@ version = __version__
 release = __version__
 
 # -- General configuration ---------------------------------------------------
+
+# The master toctree document.
+master_doc = 'index'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
